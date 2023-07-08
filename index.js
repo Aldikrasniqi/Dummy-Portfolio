a82647e717a8c1a3c87f36badbd4bab1b99a16d2
Aldikrasniqi <aldikrasniqi5@gmail.com>
2023-07-08 19:48:45 +0200

    layouts and javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+function typeWriter() {
+  const txt = 'Hello Wrld,';
+  const speed = 100;
+
+  if (i < txt.length) {
+    document.getElementById('demo').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
